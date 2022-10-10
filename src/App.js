a44decc92578763fdf3a469de25dc6af1390542f
Muhibbul Hasan <muhibbulhasan6@gmail.com>
2022-10-10 23:27:00 +0600

    header add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Blogs from './components/Blogs/Blogs';
+import Error from './components/Error/Error';
+import Statistics from './components/Statistics/Statistics';
+
+import Topics from './components/Topics/Topics';
+import Main from './layout/Main';
 
 function App() {
+  const router=createBrowserRouter([
+    {
+path:'/',
+element:<Main></Main>,
+errorElement:<Error></Error>,
+children:[
+  {
+    path:'/',
+    element:<Topics></Topics>
+  },
+  {
+    path:'/statistics',
+    element:<Statistics></Statistics>
+  },
+  {
+    path:'/blogs',
+    element:<Blogs></Blogs>
+  }
+]
+    }
+   
+ 
+  ]);
   return (
     <div className="App">
-     <h3 className='bg-red-600'>Muhib</h3>
+    <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
