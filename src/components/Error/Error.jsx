e22dfb0acf6f75ed1589b3640d350d07cb89a9ff
Muhibbul Hasan <muhibbulhasan6@gmail.com>
2022-10-11 23:35:43 +0600

    navbar clickable

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Error = () => {
   return (
@@ -11,7 +12,7 @@ const Error = () => {
 			</h2>
 			<p className="text-2xl font-semibold md:text-3xl">Sorry, we couldn't find this page.</p>
 			<p className="mt-4 mb-8 text-gray-400">But dont worry, you can find plenty of other things on our homepage.</p>
-			{/* <a rel="noopener noreferrer" href="#" className="px-8 py-3 font-semibold rounded bg-violet-400 text-gray-900">Back to homepage</a> */}
+			<Link to='/' >Back to Home</Link>
 		</div>
 	</div>
 </section>
--- src/components/Option/Option.jsx
+++ src/components/Option/Option.jsx
@@ -5,7 +5,7 @@ const Option = ({option,question,trueFalseCheck}) => {
 
   
   return (
-    <div className='border-2 p-2 bg-slate-200'>
+    <div className='border-2 p-2 bg-slate-200 hover:bg-orange-300'>
     
     <input type="radio" value={option} name={question} onClick={()=>trueFalseCheck(option)} />{option}
 
