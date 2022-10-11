0da820663d6e47bc655420eeee48fcf75daa1d03
Muhibbul Hasan <muhibbulhasan6@gmail.com>
2022-10-11 20:05:35 +0600

    home header make

--- src/components/Topics/Topics.jsx
+++ src/components/Topics/Topics.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Topic from '../Topic/Topic';
-
+import image from '../../image/image.jpg'
 
 const Topics = () => {
   const exams=useLoaderData();
   const {data}=exams;
-  // console.log(data);
+  
   return (
     <div>
       <section className="bg-gray-800 text-gray-100 mb-5">
 	<div className="container flex flex-col mx-auto lg:flex-row">
-	
+	<img src={image} alt="" className='md:w-1/5 sm:w-5/6' />
 		<div className="flex flex-col w-full p-6 lg:w-2/3 md:p-8 lg:p-12">
-			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-8 h-8 mb-8 text-violet-400">
-				<path fillRule="evenodd" d="M2.166 4.999A11.954 11.954 0 0010 1.944 11.954 11.954 0 0017.834 5c.11.65.166 1.32.166 2.001 0 5.225-3.34 9.67-8 11.317C5.34 16.67 2 12.225 2 7c0-.682.057-1.35.166-2.001zm11.541 3.708a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
-			</svg>
-			<h2 className="text-3xl font-semibold leading-none">Modern solutions to all kinds of problems</h2>
-			<p className="mt-4 mb-8 text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non voluptatum rem amet!</p>
+			
+			<h2 className="text-3xl font-semibold leading-none">Modern online Quiz Test</h2>
+			<p className="mt-4 mb-8 text-lg">We design and create original, high quality, fun quizzes, covering personality, knowledge, relationships and more. We hope that through our quizzes you’ll have fun with your friends, learn lots of interesting facts, and even get to know yourself a little better.
+
+We’ve already created dozens of popular quizzes and we’ve had a lot of fun in the process. Right now we’re working harder to bring you even more quizzes in the near future. So stay tuned.</p>
 			
 		</div>
 	</div>
